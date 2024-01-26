a3257d8076132be3a6da8c43a431a3dc6d8d930c
Kálmán Pajcsics <105979789+KalmanPA@users.noreply.github.com>
2024-01-26 17:59:01 +0100

    Movement and rotation works

--- Hungry Jo/Assets/Scripts/CharacterMovement/CharacterMovement.cs
+++ Hungry Jo/Assets/Scripts/CharacterMovement/CharacterMovement.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterMovement : MonoBehaviour
 {
     private CharacterController _characterController;
-    
+
+    [SerializeField] private GameObject _characterVisual;
+
+    private Vector3 _mousePosition;
 
     private void Awake()
     {
@@ -20,7 +25,31 @@ public class CharacterMovement : MonoBehaviour
 
         _characterController.Move(new Vector3(-yDirection, 0, xDirection) * 0.1f);
 
+        LocateMouse();
 
+        RotateVisual();
 
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    transform.position = _mousePosition;
+        //}
+
+    }
+
+    private void RotateVisual()
+    {
+        _characterVisual.transform.LookAt(_mousePosition, Vector3.up);
+    }
+
+    private void LocateMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            _mousePosition = new Vector3(hit.point.x, 1, hit.point.z);
+
+            //Debug.Log(_mousePosition.ToString());
+        }
     }
 }
